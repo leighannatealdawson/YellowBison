685af1511ffef7317f9d2cf08873bf14336efef8
leighannatealdawson <leighannatealdawson@hotmail.com>
2025-11-14 10:53:07 +0000

    Update plot to use body mass instead of bill depth
    Fixed code to plot bill depth against body mass instead of bill depth.

--- Penguin_Bills.R
+++ Penguin_Bills.R
@@ -10,8 +10,8 @@ peng <- penguins
                                     
 # Plot: Bill length vs Bill depth by species (but make it cool)
 peng %>%
-  drop_na(bill_length_mm, bill_depth_mm, species) %>%
-  ggplot(aes(x = bill_length_mm, y = bill_depth_mm, color = species, fill = species)) +
+  drop_na(bill_length_mm, , species) %>%
+  ggplot(aes(x = bill_length_mm, y = bost_mass_g, color = species, fill = species)) +
   geom_jitter(shape = 21, alpha = 0.85, width = 0.8, height = 0.8, stroke = 0.4) +
   stat_ellipse(type = "norm", linetype = "dotted", linewidth = 0.8, alpha = 0.7) +
   scale_color_manual(values = c("Adelie" = "#2b8cbe", "Chinstrap" = "#f03b20", "Gentoo" = "#2ca25f")) +
